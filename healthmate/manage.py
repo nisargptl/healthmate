2bb077ceb66f95bb03c4e0db84d125dad3294311
nisargptl <nisarg3000@gmail.com>
2024-09-28 07:25:12 -0700

    updated manage.py for db creation.

--- healthmate/manage.py
+++ healthmate/manage.py
@@ -1,8 +1,41 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+import psycopg2
+from psycopg2 import sql
 import os
 import sys
 
+def create_database_if_not_exists():
+    try:
+        db_name = os.getenv('DATABASE_NAME', 'healthmate_db')
+        db_user = os.getenv('DATABASE_USER', 'postgres')
+        db_password = os.getenv('DATABASE_PASSWORD', 'root')
+        db_host = os.getenv('DATABASE_HOST', 'localhost')
+        db_port = os.getenv('DATABASE_PORT', '5432')
+
+        conn = psycopg2.connect(
+            dbname='postgres',
+            user=db_user,
+            password=db_password,
+            host=db_host,
+            port=db_port
+        )
+        conn.autocommit = True
+        cursor = conn.cursor()
+
+        cursor.execute(sql.SQL("SELECT 1 FROM pg_catalog.pg_database WHERE datname = %s"), [db_name])
+        exists = cursor.fetchone()
+
+        if not exists:
+            cursor.execute(sql.SQL('CREATE DATABASE {}').format(sql.Identifier(db_name)))
+            print(f'Successfully created database {db_name}')
+        else:
+            print(f'Database {db_name} already exists')
+
+        cursor.close()
+        conn.close()
+    except Exception as e:
+        print(f"Error creating database: {e}")
 
 def main():
     """Run administrative tasks."""
@@ -19,4 +52,5 @@ def main():
 
 
 if __name__ == '__main__':
+    create_database_if_not_exists()
     main()
